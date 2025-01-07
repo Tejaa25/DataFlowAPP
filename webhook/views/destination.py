5f3a1da2bca1ce97ce73a74803a3943ce991ffe4
Tejaa25 <tejeshkathir07@gmail.com>
2025-01-08 01:21:02 +0530

    Destination Filter Based On Account ID

--- webhook/views/destination.py
+++ webhook/views/destination.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
 from webhook.models import Destination
 from webhook.serializers import DestinationSerializer
 
@@ -8,3 +9,5 @@ class DestinationModelApiViewSet(ModelViewSet):
 
     serializer_class = DestinationSerializer
     queryset = Destination.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["account"]
